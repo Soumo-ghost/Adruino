75e5ddd4989f4dac5948a82c50b48f4cb826d992
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-12-10 19:15:52 +0530

    pattern

--- 01_Arduini/src/main.cpp
+++ 01_Arduini/src/main.cpp
@@ -1,27 +1,58 @@
-#include<Arduino.h>
-byte pin0=0;
-byte pin1=1;
-byte pin2=2;
-byte pin3=3;
-byte pin4=4;
+#include <Wire.h>
+#include <Arduino.h>
 
-void setup()
-{
-  pinMode(pin0, OUTPUT);
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(pin3, OUTPUT);
-  pinMode(pin4, OUTPUT);
+const int SLAVE_ADDRESS = 9;
+const int LED_PINS[] = {9, 10, 11}; 
+const int NUM_STEPS = 5;
+const int MASTER_PATTERNS[] = {0, 1, 2, 3, 7};
+int currentStep = 0;
+
+void updateMasterLEDs(int patternValue) {
+  digitalWrite(LED_PINS[0], (patternValue & 0b001)); 
+  digitalWrite(LED_PINS[1], (patternValue & 0b010)); 
+  digitalWrite(LED_PINS[2], (patternValue & 0b100)); 
 }
 
-void loop(){
-for (int i = 0; i < 32; ++i) {
-   
-    digitalWrite(pin0, i & 1); 
-    digitalWrite(pin1, (i >> 1) & 1);
-    digitalWrite(pin2, (i >> 2) & 1);
-    digitalWrite(pin3, (i >> 3) & 1); 
-	digitalWrite(pin4, (i >> 4) & 1); 
+void sendCommandToSlave(int step) {
+  Wire.beginTransmission(SLAVE_ADDRESS);
+  Wire.write(step); 
+  Wire.endTransmission();
+}
+
+void printPattern(int pattern) {
+  Serial.print((pattern & 0b100) ? "1" : "0");
+  Serial.print((pattern & 0b010) ? "1" : "0");
+  Serial.print((pattern & 0b001) ? "1" : "0");
+}
+
+void setup() {
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Master Ready. LEDs: 9, 10, 11");
+}
+
+void loop() {
+  int masterPattern = MASTER_PATTERNS[currentStep];
+  
+ 
+  updateMasterLEDs(masterPattern);
+
+ 
+  sendCommandToSlave(currentStep);
+
+
+  Serial.print("Step ");
+  Serial.print(currentStep);
+  Serial.print(" | Master: ");
+  printPattern(masterPattern);
+  Serial.print(" | Slave Pattern Index: ");
+  Serial.println(currentStep); 
+
   delay(1000);
-}
+
+  
+  currentStep = (currentStep + 1) % NUM_STEPS;
 }
---
+++ 01_Arduini/src/main2.cpp
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <Arduino.h>
+const int SLAVE_ADDRESS = 9;
+const int LED_PINS[] = {9, 10, 11}; 
+
+
+const int SLAVE_PATTERNS[] = {0, 1, 2, 3, 0};
+
+
+void receiveEvent(int byteCount) {
+  if (Wire.available()) {
+    int step = Wire.read(); 
+
+    if (step >= 0 && step < sizeof(SLAVE_PATTERNS) / sizeof(SLAVE_PATTERNS[0])) {
+      int patternValue = SLAVE_PATTERNS[step];
+      
+     
+      digitalWrite(LED_PINS[0], (patternValue & 0b001)); 
+      digitalWrite(LED_PINS[1], (patternValue & 0b010)); 
+      digitalWrite(LED_PINS[2], (patternValue & 0b100));
+    }
+  }
+}
+
+void setup() {
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+    digitalWrite(LED_PINS[i], LOW); 
+  }
+  
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+}
